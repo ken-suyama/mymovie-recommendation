e195e226f53869410691d4c1ea887ae6f7c9bbfe
ken-suyama <kh_6bbl@i.softbank.jp>
2020-02-05 02:31:43 +0900

    変更

--- mymovie/settings.py
+++ mymovie/settings.py
@@ -45,8 +45,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mymovie.wsgi.application'
+WSGI_APPLICATION = 'mymovie-recommendation.wsgi.application'
 
 
 # Database
