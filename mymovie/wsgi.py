097afced859e8af74f71c679a86a2c5f1587a427
ken-suyama <kh_6bbl@i.softbank.jp>
2020-02-05 14:45:15 +0900

    Update wsgi.py

--- mymovie/wsgi.py
+++ mymovie/wsgi.py
@@ -11,9 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mymovie.settings')
 
 # application = get_wsgi_application()
